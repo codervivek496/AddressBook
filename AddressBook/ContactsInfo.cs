b36a1bb4145adbc696ea23f56eb38d491eff5770
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-21 07:30:51 -0700

    Create contacts in address book

---
+++ AddressBook/ContactsInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class ContactsInfo
+    {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string email;
+        public string zip;
+        public long phoneNumber;
+
+        public ContactsInfo(string aFirstName, string aLastName, string aAddress, string aCity, string aState, string aEmail, string aZip, long aPhoneNumber)
+        {
+            //Set value of class variable from the constructor
+            this.firstName = aFirstName;
+            this.lastName = aLastName;
+            this.address = aAddress;
+            this.city = aCity;
+            this.state = aState;
+            this.email = aEmail;
+            this.zip = aZip;
+            this.phoneNumber = aPhoneNumber;
+        }
+
+        //Display the contacts information
+        public void DisplayInformation()
+        {
+            Console.WriteLine("First Name: " + this.firstName);
+            Console.WriteLine("Last Name: " + this.lastName);
+            Console.WriteLine("Address: " + this.address);
+            Console.WriteLine("City: " + this.city);
+            Console.WriteLine("State: " + this.address);
+            Console.WriteLine("Email id: " + this.email);
+            Console.WriteLine("Zip code: " + this.zip);
+            Console.WriteLine("Phoner number: " + this.phoneNumber);
+        }
+
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AddressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\t\tWelcome to Address Book Program");
+            int choice;
+            do
+            {
+                Console.WriteLine("\n1. Contact details in adress book");
+
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Contact details are as shown below");
+                        //Passing the input to Constructor parameter through onject
+                        ContactsInfo contactInfo = new ContactsInfo("Vivek", "Kumar", "Somajiguda", "Hyderabad", "Telangana", "viveksinghssm496@gmai.com", "500082", 7004437837);
+                        contactInfo.DisplayInformation();
+                        break;
+                    default:
+                        Console.WriteLine("Enter corect choice");
+                        break;
+                }
+            }while (choice != 0);
+        }
+    }
+}
