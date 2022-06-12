a190a86823b22187bbfb0ebe3f6f3bb2e52e3ce5
Vivek kumar <viveksinghssm496@gmail.com>
2022-06-12 20:24:07 +0530

    Addedd to SearchPersonByCityOrState

--- AddressBook/MultipleAddressBook.cs
+++ AddressBook/MultipleAddressBook.cs
@@ -244,7 +244,7 @@ namespace AddressBook
                             break;
                         case 6:
                             Console.Write("Enter new zip code : ");
-                            string newZip = Console.ReadLine();
+                           String newZip = Console.ReadLine();
                             detail.zip = newZip;
                             break;
                         case 7:
@@ -291,6 +291,45 @@ namespace AddressBook
                 else
                 {
                     Console.WriteLine("Wrong credentials");
+                }
+            }
+        }
+
+        //To search person by city name
+        public void SearchPersonByCity()
+        {
+            Console.Write("Enter the city to search : ");
+            string cityName = Console.ReadLine();
+            foreach (KeyValuePair<string, int> keyValuePair in AddrBook)
+            {
+                int count = 0;
+                foreach (ContactDetails details in contactDetailsList[count])
+                {
+                    if (details.city == cityName)
+                    {
+                        Console.WriteLine("Person " + details.firstName + " from city : " + details.city + " is present in Address book : " + keyValuePair.Key);
+                    }
+                    count++;
+                }
+            }
+        }
+
+        //To search person by state name
+        public void SearchPersonByState()
+        {
+            Console.Write("Enter the state to search : ");
+            string stateName = Console.ReadLine();
+            foreach (KeyValuePair<string, int> keyValuePair in AddrBook)
+            {
+                int count = 0;
+                foreach (ContactDetails details in contactDetailsList[count])
+                {
+                    if (details.city == stateName)
+                    {
+                        Console.WriteLine("Person " + details.firstName + " from state : " + details.state + " is present in Address book : " + keyValuePair.Key);
+                        break;
+                    }
+                    count++;
                 }
             }
         }
